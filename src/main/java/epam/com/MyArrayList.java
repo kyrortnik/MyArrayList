eb104cd626e57ab2d0c2a917ac3b7a8cb26d0c15
Mikhail Piatrenka <kyrortnik97@gmail.com>
2021-08-20 18:44:59 +0300

    first commit

---
+++ src/main/java/epam/com/MyArrayList.java
@@ -0,0 +1,354 @@
+package epam.com;
+
+import java.util.*;
+
+public class MyArrayList<E> extends AbstractList<E> implements List<E>, Cloneable {
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+
+    private static final Object[] EMPTY_ELEMENTDATA = {};
+
+
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+
+
+    transient  Object[] elementData;
+
+
+    private int size;
+
+
+    public MyArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.elementData = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.elementData = EMPTY_ELEMENTDATA;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: "+
+                    initialCapacity);
+        }
+    }
+
+
+    public MyArrayList() {
+        this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+    }
+
+
+    public MyArrayList(Collection<? extends E> c) {
+        Object[] a = c.toArray();
+        if ((size = a.length) != 0) {
+            if (c.getClass() == ArrayList.class) {
+                elementData = a;
+            } else {
+                elementData = Arrays.copyOf(a, size, Object[].class);
+            }
+        } else {
+            elementData = EMPTY_ELEMENTDATA;
+        }
+    }
+
+
+    /**
+     * Methods which enlarge elementData in case we need to add elements over its size.
+     */
+
+    private void ensureCapacity(int minCapacity) {
+        ensureExplicitCapacity(calculateCapacity(elementData, minCapacity));
+    }
+
+
+    private static int calculateCapacity(Object[] elementData, int minCapacity) {
+        if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+            return Math.max(DEFAULT_CAPACITY, minCapacity);
+        }
+        return minCapacity;
+    }
+
+    private void ensureExplicitCapacity(int minCapacity) {
+        modCount++;
+
+        if (minCapacity - elementData.length > 0)
+            grow(minCapacity);
+    }
+
+
+    /**
+     * The maximum size of array to allocate.
+     */
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
+
+   private void grow(int minCapacity) {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        if (newCapacity - minCapacity < 0)
+            newCapacity = minCapacity;
+        if (newCapacity - MAX_ARRAY_SIZE > 0)
+            newCapacity = hugeCapacity(minCapacity);
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0)
+            throw new OutOfMemoryError();
+        return (minCapacity > MAX_ARRAY_SIZE) ?
+                Integer.MAX_VALUE :
+                MAX_ARRAY_SIZE;
+    }
+
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+
+    public void trimToSize() {
+        modCount++;
+        if (size < elementData.length) {
+            elementData = (size == 0)
+                    ? EMPTY_ELEMENTDATA
+                    : Arrays.copyOf(elementData, size);
+        }
+    }
+
+
+
+    public int indexOf(Object o){
+        if (o == null){
+            for (int i =0;i< size;i++)
+                if (elementData[i] == null)
+                    return i;
+        } else {
+            for (int i = 0;i<size;i++)
+                if (o.equals(elementData[i]))
+                    return i;
+        }
+        return -1;
+    }
+
+
+    public int lastIndexOf(Object o) {
+        if (o == null) {
+            for (int i = size-1; i >= 0; i--)
+                if (elementData[i] == null)
+                    return i;
+        } else {
+            for (int i = size-1; i >= 0; i--)
+                if (o.equals(elementData[i]))
+                    return i;
+        }
+        return -1;
+    }
+
+
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: "+index+", Size: "+size;
+    }
+
+
+
+    /**
+     * Methods sets element to replace an existing one and return replaced element.
+     * */
+
+    public E set(int index, E element) {
+        rangeCheck(index);
+
+        E oldValue = elementData(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+
+    /**
+     * Method adds an element to the end of MyArrayList.
+     */
+    public boolean add(E e) {
+        ensureCapacity(size + 1);  // Increments modCount!!
+        elementData[size++] = e;
+        return true;
+    }
+
+
+    /**
+     * Method adds element to an index position without overwriting existing value.
+     */
+
+    public void add(int index, E element) {
+        rangeCheckForAdd(index);
+
+        ensureCapacity(size + 1);  // Increments modCount!!
+        System.arraycopy(elementData, index, elementData, index + 1,
+                size - index);
+        elementData[index] = element;
+        size++;
+    }
+
+
+    /**
+     * Method removes value from MyArrayList.
+     **/
+
+    public E remove(int index) {
+        rangeCheck(index);
+
+        modCount++;
+        E oldValue = elementData(index);
+
+        int numMoved = size - index - 1;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index+1, elementData, index,
+                    numMoved);
+        elementData[--size] = null; // clear to let GC do its work
+
+        return oldValue;
+    }
+
+
+    /**
+     * Method removes an object using fastRemove().
+     * */
+    public boolean remove(Object o){
+        if (o == null){
+            for (int i = 0;i<size;i++)
+                if (elementData[i] == null) {
+                    fastRemove(i);
+                    return true;
+                }
+        } else {
+            for (int i = 0;i<size;i++)
+                if (o.equals(elementData[i])) {
+                    fastRemove(i);
+                    return true;
+                }
+        }
+        return false;
+    }
+
+
+    private void fastRemove(int index) {
+        modCount++;
+        int numMoved = size - index - 1;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index+1, elementData, index,
+                    numMoved);
+        elementData[--size] = null; // GC will delete object without an active link.
+    }
+
+    private void rangeCheckForAdd(int index) {
+        if (index > size || index < 0)
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+
+    private void rangeCheck(int index) {
+        if (index >= size)
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+
+    @SuppressWarnings("unchecked")
+    E elementData(int index) {
+        return (E) elementData[index];
+    }
+
+   @Override
+    public E get(int index) {
+        rangeCheck(index);
+        return elementData(index);
+    }
+
+
+    /**
+     * Creating shallow clone.
+     * */
+
+    @Override
+    public Object clone() {
+        try {
+            MyArrayList<?> v = (MyArrayList<?>) super.clone();
+            v.elementData = Arrays.copyOf(elementData, size);
+            v.modCount = 0;
+            return v;
+        } catch (CloneNotSupportedException e) {
+            throw new InternalError(e);
+        }
+    }
+
+
+    public Object[] toArray() {
+        return Arrays.copyOf(elementData, size);
+    }
+
+
+
+    public void clear() {
+        modCount++;
+
+        // clear to let GC do its work
+        for (int i = 0; i < size; i++)
+            elementData[i] = null;
+
+        size = 0;
+    }
+
+
+    public boolean addAll(Collection<? extends E> c) {
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        ensureCapacity(size + numNew);  // Increments modCount
+        System.arraycopy(a, 0, elementData, size, numNew);
+        size += numNew;
+        return numNew != 0;
+    }
+
+
+    public boolean addAll(int index, Collection<? extends E> c) {
+        rangeCheckForAdd(index);
+
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        ensureCapacity(size + numNew);  // Increments modCount
+
+        int numMoved = size - index;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index, elementData, index + numNew,
+                    numMoved);
+
+        System.arraycopy(a, 0, elementData, index, numNew);
+        size += numNew;
+        return numNew != 0;
+    }
+
+
+
+    protected void removeRange(int fromIndex, int toIndex) {
+        modCount++;
+        int numMoved = size - toIndex;
+        System.arraycopy(elementData, toIndex, elementData, fromIndex,
+                numMoved);
+
+        // clear to let GC do its work
+        int newSize = size - (toIndex-fromIndex);
+        for (int i = newSize; i < size; i++) {
+            elementData[i] = null;
+        }
+        size = newSize;
+    }
+
+
+
+}
